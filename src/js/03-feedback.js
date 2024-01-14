e58e871d3f4dd82413468c237ffe07062c74402c
Iurii <iamyurkas@gmail.com>
2024-01-14 22:44:50 +0000

    Added 3 hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const doc = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+}
+
+doc.form.addEventListener('input', throttle(
+  () => {
+    const email = doc.email.value;
+    const message = doc.message.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify({
+      email,
+      message
+    }));
+  },
+  500
+));
+
+window.addEventListener('load', () => {
+  const formFields = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (formFields) {
+    doc.email.value = formFields.email;
+    doc.message.value = formFields.message;
+  }
+});
+
+doc.form.addEventListener('submit', () => {
+  console.log({
+    email: doc.email.value,
+    message: doc.message.value,
+  });
+    
+  doc.email.value = '';
+  doc.message.value = '';
+    
+  localStorage.clear();
+});

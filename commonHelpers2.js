8ff0373bde554e8d4c58e5455d2872ef2502012f
iamyurkas <iamyurkas@gmail.com>
2024-01-14 22:17:30 +0000

    Deploying to gh-pages from @ iamyurkas/goit-advancedjs-hw-01@e81cf8107f89e7610a8ddd5f6864c2f4ea95f00f 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as o}from"./assets/vendor-ceec3c52.js";const a=document.querySelector("iframe"),e=new r(a),l=function({seconds:t}){localStorage.setItem("videoplayer-current-time",t)};e.on("timeupdate",o(l,1e3));const m=+localStorage.getItem("videoplayer-current-time");e.setCurrentTime(m);
+//# sourceMappingURL=commonHelpers2.js.map
